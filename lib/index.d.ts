d249e884c9fb15772da6fa3a83cf309d7f416a0d
JOY-LAPTOP-6\drag_ <kevintan.me@gmail.com>
2024-02-27 13:57:26 +0700

    add declaration

---
+++ lib/index.d.ts
@@ -0,0 +1,3 @@
+export declare function sum(a: number, b: number): number;
+export declare function sub(a: number, b: number): number;
+export declare function sum3(a: number, b: number, c: number): number;
---
+++ lib/src/lc.d.ts
@@ -0,0 +1,2 @@
+import 'dotenv/config';
+export declare function getJoke(topic: string): Promise<string>;
